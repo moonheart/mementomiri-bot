dc469da815a11d735fcdcd08f8055c5d3fc8b9f6
moonheart <themoonheart@outlook.com>
2024-06-04 23:31:51 +0800

    add files

--- MementoMori.BotServer/Program.cs
+++ MementoMori.BotServer/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+using BotServer;
+using MementoMori.BotServer.Options;
+using MementoMori.Option;
 
-Console.WriteLine("Hello, World!");
+namespace MementoMori.BotServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.ConfigureWritable<AuthOption>(context.Configuration.GetSection("Auth"));
+                    services.ConfigureWritable<BotOptions>(context.Configuration.GetSection("Bot"));
+                    services.ConfigureWritable<GameConfig>(context.Configuration.GetSection("Game"));
+                    services.AddHttpClient();
+                    services.AddSingleton<MementoNetworkManager>();
+                    services.Discover();
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
